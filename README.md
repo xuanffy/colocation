05192f0b4dacdf6807f288ef41ccc9efbe691dfd
yuxuan <sdzhangyuxuan@gmail.com>
2021-03-02 21:36:35 -0800

    Data and Cleaning
    Prison and superfund data are ready for analysis

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-colocation
+STA 260 toxic prison project
+
+Part of the code and data is from https://github.com/Carceral-Ecologies/Carceral-Proximity-Analysis by Professor Lindsay Poirier.
+
---
+++ data_clean.R
@@ -0,0 +1,17 @@
+# This part is from Lidsay's GitHub code file
+library(sf)
+library(tidyverse)
+pb <- st_read("data/Prison_Boundaries.shp", stringsAsFactors = FALSE)
+pb_sf <- st_transform(pb, crs = 4269)
+#Reduce prisons from polygons to points (centroids) to reduces distance calculation times
+pb_sf <- st_transform(pb_sf, crs = 32617) %>% #convert to utm for calculating centroids
+  st_centroid() %>% #centroids from original multipolygons
+  st_transform(crs = 4269) #back to 4269
+#longitude and latitude information are stored in "geometry" feature
+
+sfs <- read.csv("data/sf.csv", stringsAsFactors = FALSE) 
+sfs <- sfs %>% filter(!is.na(LONGITUDE83))
+pb_crs <- st_crs(pb_sf)
+sfs_sf <- st_as_sf(sfs, coords = c("LONGITUDE83", "LATITUDE83"), crs = pb_crs, na.fail = FALSE)
+sfs_sf <- sfs_sf %>% rename(ID = X)
+#longitude and latitude information are stored in "geometry" feature
