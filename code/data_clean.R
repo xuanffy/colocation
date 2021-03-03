1f0689809460fc853e58d138daa53b6d82f135f5
tkl96 <til41@pitt.edu>
2021-03-02 22:51:50 -0800

    localtion data saving
    pb_loc: location information for prisoner bound
    sfs_loc: location information for superfund sites

--- code/data_clean.R
+++ code/data_clean.R
@@ -8,6 +8,8 @@ pb_sf <- st_transform(pb_sf, crs = 32617) %>% #convert to utm for calculating ce
   st_centroid() %>% #centroids from original multipolygons
   st_transform(crs = 4269) #back to 4269
 #longitude and latitude information are stored in "geometry" feature
+pb_loc<-pb_sf%>% select(FID,geometry)
+
 
 sfs <- read.csv("data/sf.csv", stringsAsFactors = FALSE) 
 sfs <- sfs %>% filter(!is.na(LONGITUDE83))
@@ -15,3 +17,9 @@ pb_crs <- st_crs(pb_sf)
 sfs_sf <- st_as_sf(sfs, coords = c("LONGITUDE83", "LATITUDE83"), crs = pb_crs, na.fail = FALSE)
 sfs_sf <- sfs_sf %>% rename(ID = X)
 #longitude and latitude information are stored in "geometry" feature
+sfs_loc<-sfs_sf%>% select(ID,geometry)
+
+###The location info are saved in local.rda file
+save(pb_loc,sfs_loc,file = "data/local.rda")
+rm(list=ls())
+load("data/local.rda")
